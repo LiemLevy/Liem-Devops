53d9e0455cc36ae79c7d4fcd5b8413dbd0bfcfb4
Liem Levy <liem36194@gmail.com>
2025-04-05 12:09:56 +0300

    LiemProject v2

--- LiemProject/vmauto.py
+++ LiemProject/vmauto.py
@@ -1,8 +1,9 @@
 def get_vm_input():
     name = input("Enter VM name: ")
-    os = input("Enter OS: ")
+    os = input("Enter OS (Linux Ubuntu / Windows 10):  ")
     cpu = int(input("Enter number of CPUs: "))
     ram = int(input("Enter amount of RAM (GB): "))
     return {"name": name, "os": os, "cpu": cpu, "ram": ram}
 
+ןif os == "Linux Ubuntu": 
 get_vm_input()
