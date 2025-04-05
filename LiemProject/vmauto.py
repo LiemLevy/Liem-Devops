42ac04a05c9e981568e713f7d12d553dc18b9648
Liem Levy <liem36194@gmail.com>
2025-04-05 15:52:09 +0300

    LiemProject v3.0.0

--- LiemProject/vmauto.py
+++ LiemProject/vmauto.py
@@ -1,3 +1,18 @@
+import json
+import jsonschema
+from src.machine import Machine  # אנחנו מייבאים את המחלקה Machine
+
+vm_schema = {
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "os": {"type": "string", "enum": ["Linux Ubuntu", "Windows 10", "Linux", "Windows", "windows", "linux"]},
+        "cpu": {"type": "integer", "minimum": 1},
+        "ram": {"type": "integer", "minimum": 1}
+    },
+    "required": ["name", "os", "cpu", "ram"]
+}
+
 def get_vm_input():
     name = input("Enter VM name: ")
     if not name.strip():
@@ -29,9 +44,24 @@ def get_vm_input():
 
     return {"name": name, "os": os, "cpu": cpu, "ram": ram}
 
+
+def save_vm_data_to_json(vm_data):
+    
+    try:
+        jsonschema.validate(instance=vm_data, schema=vm_schema)
+        print("✅ JSON schema validation passed.")
+        with open('LiemProject/instances.json', 'w') as f:
+            json.dump(vm_data, f, indent=4)
+    except Exception as e:
+        print("❌ JSON schema validation failed")
+
+
 vm_data = get_vm_input()
 if vm_data:
+    machine = Machine(vm_data['name'], vm_data['os'], vm_data['cpu'], vm_data['ram'])
     print("✅ Good input")
     print(vm_data)
 else:
     print("⛔️ Bad input")
+
+save_vm_data_to_json(vm_data)
