c04b8ffe9009694745f83616ffede4604a3c1bd8
Liem Levy <liem36194@gmail.com>
2025-04-05 12:32:56 +0300

    good input

--- LiemProject/vmauto.py
+++ LiemProject/vmauto.py
@@ -1,9 +1,37 @@
 def get_vm_input():
     name = input("Enter VM name: ")
+    if not name.strip():
+        print("❌ VM name cannot be empty.")
+        return False
+
     os = input("Enter OS (Linux Ubuntu / Windows 10):  ")
-    cpu = int(input("Enter number of CPUs: "))
-    ram = int(input("Enter amount of RAM (GB): "))
+    if os not in ["Linux Ubuntu", "Windows 10", "Linux", "Windows","windows","linux"]:
+        print("❌ The operating system must be 'Linux Ubuntu' or 'Windows 10'.")
+        return False
+
+    try:
+        cpu = int(input("Enter number of CPUs: "))
+        if cpu <= 0:
+            print("❌ The number of cores (CPU) must be positive.")
+            return False
+    except ValueError:
+        print("❌ CPU must be an integer.")
+        return False
+
+    try:
+        ram = int(input("Enter amount of RAM (GB): "))
+        if ram <= 0:
+            print("❌ The memory (RAM) must be positive.")
+            return False
+    except ValueError:
+        print("❌ RAM must be an integer.")
+        return False
+
     return {"name": name, "os": os, "cpu": cpu, "ram": ram}
 
-ןif os == "Linux Ubuntu": 
-get_vm_input()
+vm_data = get_vm_input()
+if vm_data:
+    print("✅ Good input")
+    print(vm_data)
+else:
+    print("⛔️ Bad input")
